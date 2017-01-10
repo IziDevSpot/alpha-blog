08b89ad219e5847ac7fb59fe1e6914d55e6b63a7
Ismael Chery <izidevspot@gmail.com>
2017-01-10 16:56:04 +0000

    complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 before_action :set_article, only: [:edit,:update,:show,:destroy]
+before_action :require_user, except: [:index,:show]
+before_action :require_same_user, only: [:edit, :update, :destroy]
+
 def index
 
 @articles=Article.paginate(page: params[:page] , per_page: 3)
@@ -9,12 +12,12 @@ def new
 end 
 def edits
 #@article=Article.find(params[:id])
-end
+end 
 
 def create
 
 @article=Article.new(article_params)
- @article.user= User.first
+ @article.user= current_user
 if@article.save
 flash[:success]="Article was succesfully created"
 redirect_to article_path(@article)
@@ -24,7 +27,7 @@ render'new'
 
 end
 
-end
+end 
 def update
 #@article=Article.find(params[:id])
 
@@ -57,4 +60,12 @@ def article_params
 params.require(:article).permit(:title,:description)
 end
 
-end 
+def require_same_user
+    if current_user != @article.user
+    flash[:danger]="you can only edit or delete your own article"
+    redirect_to root_path
+end
+end
+end
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+ 
+before_action :set_user, only: [:edit,:update,:show]
+before_action :require_same_user, only: [:edit,:update]
 
 def index
  @users=User.paginate(page: params[:page] , per_page: 2)
@@ -21,11 +24,11 @@ end
  end  
  end
  def edit
-  @user=User.find(params[:id])   
+  
  end
  
  def update
-     @user=User.find(params[:id])
+ 
      
      if @user.update(user_params)
          flash[:success]="your account was updated successfully"
@@ -35,7 +38,7 @@ end
  end
  
  def show
- @user=User.find(params[:id])
+
  @user_articles=@user.articles.paginate(page: params[:page] , per_page: 2)
  end
 
@@ -43,5 +46,14 @@ private
 def user_params
     params.require(:user).permit(:username,:email,:password)
 end
+def set_user
+   @user=User.find(params[:id]) 
+end
 
+def require_same_user
+if current_user !=@user
+flash[:danger]= "you can only edit your own accound"
+redirect_to root_path
+end
+end
 end
