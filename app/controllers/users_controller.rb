fcb7599ff36908558c224f08130f9fd2e37f7621
Ismael Chery <izidevspot@gmail.com>
2017-01-10 20:59:54 +0000

    added sign up upon sign in fonctionality in the user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,8 +15,9 @@ end
  def create
  @user=User.new(user_params) 
  if @user.save
+     session[:user_id]=@user.id
    flash[:success]="welcome to the alpha blog #{@user.username}"
-   redirect_to articles_path
+   redirect_to user_path(@user)
  else
  
  render'new'
@@ -32,7 +33,7 @@ end
      
      if @user.update(user_params)
          flash[:success]="your account was updated successfully"
-         redirect_to articles_path
+         redirect_to ar ticles_path
       else render 'edit'
      end
  end
