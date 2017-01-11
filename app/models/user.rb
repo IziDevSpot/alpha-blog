aef916a5e0b3f0741a6386ef9030f498436c62ad
Ismael Chery <izidevspot@gmail.com>
2017-01-11 23:51:19 +0000

    Add admin user fonctionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User <ActiveRecord::Base
-has_many :articles
+has_many :articles, dependent: :destroy 
 before_save{self.email=email.downcase}
 validates :username, presence: true,format: { without: /\s/, message: "must contain no spaces" }, 
 uniqueness: {case_sensitive:false}, 
