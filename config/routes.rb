b430e02c2b05c697f73d13c7fd15357c80593af3
Ismael Chery <izidevspot@gmail.com>
2017-01-25 13:28:34 -0500

    Add categories and test

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ get 'signup', to: 'users#new'
  get 'login', to: 'sessions#new'
  post 'login', to: 'sessions#create'
  delete 'logout', to: 'sessions#destroy'
+
+ resources :categories,except:[:destroy]
+ 
  
 end
